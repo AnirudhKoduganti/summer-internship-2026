3e94280258bf8f1af7fce2d145c2a0b09cf5c982
AnirudhKoduganti <ani.koduganti@gmail.com>
2026-07-17 20:23:02 -0400

    Edited Week 1 Project

---
+++ week-1-internship/README.md
@@ -0,0 +1,23 @@
+# summer-internship-2026
+
+What I built:
+
+I built a book explorer which analyzes five books and gives statistics about each of them. It first loads in popular texts from Project Gutenberg and then cleans out those texts by removing punctuation, stop words, and keeping everything lower case.
+
+By calling the common_freq(book) method, you will be able to count the top 20 most common words in a book you choose from the five.
+
+Next, by calling the compare_books(book1, book2) method, you are able to see which two books you choose are the most similar in vocabulary.
+
+You can also see which book has the largest unique word vocabulary by printing out the length of unique_words(book) function.
+
+Next, you can calculate and find out which book has the highest word diversity by calling the calculate_word_diversity(book) method.
+
+Lastly, you can use a search tool which takes a word or phrase and returns the top 3 books where it appears most frequently as well as the number of times in those books it appears. To use this feature, you simply call search("any word", book).
+
+How to run: To run this program you need to call the methods and print out those methods to the console to see the results. For example, if I wanted to see which book has the highest word diveristy, I would print out the following:
+
+print("Frankenstein:", calculate_word_diversity(frankText)) print("Alice:", calculate_word_diversity(aliceText)) print("Moby Dick:", calculate_word_diversity(mobyText)) print("Pride and Prejudice:", calculate_word_diversity(prideText)) print("Sherlock Holmes:", calculate_word_diversity(sherlockText))
+
+I can then run the code and see based on the results which book has the highest word diversity. I have all the print statements inside my code, so you can delete them and test them yourself.
+
+3 interesting things: I found... Moby Dick has the largest unique vocabulary Alice has the highest word diversity Moby Dick and Pride and Prejudice share the most words in common
---
+++ week-1-internship/main.py
@@ -0,0 +1,197 @@
+# load texts 
+
+with open("books/frankenstein.txt", "r", encoding="utf-8") as file: 
+    frankText = file.read()
+
+with open("books/alice.txt", "r", encoding="utf-8") as file: 
+    aliceText = file.read()
+
+with open("books/moby.txt", "r", encoding="utf-8") as file:
+    mobyText = file.read()
+
+with open("books/prideandprejudice.txt", "r", encoding="utf-8") as file:
+    prideText = file.read()
+
+with open("books/sherlock.txt", "r", encoding="utf-8") as file:
+    sherlockText = file.read() 
+
+stop_words = {
+    "a", "about", "above", "after", "again", "against", "all", "am",
+    "an", "and", "any", "are", "as", "at", "be", "because", "been",
+    "before", "being", "below", "between", "both", "but", "by",
+    "can", "could", "did", "do", "does", "doing", "down", "during",
+    "each", "few", "for", "from", "further", "had", "has", "have",
+    "having", "he", "her", "here", "hers", "herself", "him",
+    "himself", "his", "how", "i", "if", "in", "into", "is", "it",
+    "its", "itself", "just", "me", "more", "most", "my", "myself",
+    "no", "nor", "not", "now", "of", "off", "on", "once", "only",
+    "or", "other", "our", "ours", "ourselves", "out", "over", "own",
+    "same", "she", "should", "so", "some", "such", "than", "that",
+    "the", "their", "theirs", "them", "themselves", "then", "there",
+    "these", "they", "this", "those", "through", "to", "too", "under",
+    "until", "up", "very", "was", "we", "were", "what", "when",
+    "where", "which", "while", "who", "whom", "why", "will", "with",
+    "would", "you", "your", "yours", "yourself", "yourselves", "said", 
+    "mr", "mrs", "one", "two", "last", "ye", "upon", "now", "yet", "come",
+    "never", "must", "shall", "may", "might", "must", "could", "would",
+    "see"
+} 
+
+books = {
+        "Frankenstein": frankText, 
+        "Alice": aliceText, 
+        "Moby Dick": mobyText, 
+        "Pride and Prejudice": prideText,
+        "Sherlock Holmes": sherlockText
+    }
+
+def clean_text(book):
+    punctuation = '''!()-[]{};:'",<>./?@#$%^&*_~'''
+    lower = book.lower()
+    
+    lower = lower.split() 
+
+    cleaned = []
+
+    for word in lower:
+        for char in punctuation:
+            word = word.replace(char, "")
+        cleaned.append(word)
+
+    lower = cleaned 
+
+    filtered = []
+
+    for word in lower:
+        if word not in stop_words:
+            filtered.append(word)
+
+    lower = filtered 
+
+    return lower
+
+
+def word_freq(book):
+    cleaned_text = clean_text(book)
+    word_count = {}
+
+    for word in cleaned_text:
+        if word in word_count: 
+            word_count[word] += 1  
+        else:
+            word_count[word] = 1 
+
+    word_count = dict(sorted(word_count.items(), key = lambda item: item[1], reverse = True))
+
+    return word_count
+
+
+def common_freq(book):
+    word_count = word_freq(book)
+
+    common_words = {}
+    counter = 0
+    for word, count in word_count.items():
+        common_words[word] = count
+        counter += 1
+
+        if(counter == 20):
+            break
+
+    return common_words
+
+
+
+# similar means which two books have the most unique words in common 
+def unique_words(book):
+    word_count = word_freq(book)
+
+    unique_words = set()
+
+    for word in word_count: 
+        unique_words.add(word)
+
+    return unique_words 
+
+def compare_books(book1, book2):
+    unique_words1 = unique_words(book1)
+    unique_words2 = unique_words(book2)
+
+    shared = 0
+
+    for word in unique_words1:
+        if word in unique_words2:
+            shared += 1
+
+    return shared
+
+
+def calculate_word_diversity(book):
+    unique = unique_words(book)
+    total_words = len(clean_text(book))
+
+    word_diversity = len(unique) / total_words
+
+    return word_diversity 
+
+def search(query, books):
+
+    query = query.lower()
+
+    results = {}
+
+    for title, text in books.items():
+        results[title] = text.lower().count(query)
+
+    results = dict(sorted(results.items(), key = lambda item: item[1], reverse = True))
+
+    top = {}
+
+    counter = 0
+
+    for title, count in results.items():
+        top[title] = count
+        counter += 1
+
+        if(counter == 3): 
+            break 
+
+    return top 
+
+# after checking every pair, Moby Dick and Pride and Prejudice share the most words in common
+
+if __name__ == "__main__":
+
+    print("Frankenstein vs Alice:", compare_books(frankText, aliceText), " shared words")
+    print("Frankenstein vs Moby Dick:", compare_books(frankText, mobyText), " shared words")
+    print("Frankenstein vs Pride and Prejudice:", compare_books(frankText, prideText), " shared words")
+    print("Frankenstein vs Sherlock Holmes:", compare_books(frankText, sherlockText), " shared words")
+
+    print("Alice vs Moby Dick:", compare_books(aliceText, mobyText), " shared words")
+    print("Alice vs Pride and Prejudice:", compare_books(aliceText, prideText), " shared words")
+    print("Alice vs Sherlock Holmes:", compare_books(aliceText, sherlockText), " shared words")
+
+    print("Moby Dick vs Pride and Prejudice:", compare_books(mobyText, prideText), " shared words")
+    print("Moby Dick vs Sherlock Holmes:", compare_books(mobyText, sherlockText), " shared words")
+
+    print("Pride and Prejudice vs Sherlock Holmes:", compare_books(prideText, sherlockText), " shared words")
+
+
+    # moby dick has the most unique words 
+    print("Frankenstein:", len(unique_words(frankText)), " unique words")
+    print("Alice:", len(unique_words(aliceText)), " unique words")
+    print("Moby Dick:", len(unique_words(mobyText)), " unique words")
+    print("Pride and Prejudice:", len(unique_words(prideText)), " unique words")
+    print("Sherlock Holmes:", len(unique_words(sherlockText)), " unique words")
+
+    # Alice has the highest word diversity 
+    print("Frankenstein:", calculate_word_diversity(frankText))
+    print("Alice:", calculate_word_diversity(aliceText))
+    print("Moby Dick:", calculate_word_diversity(mobyText))
+    print("Pride and Prejudice:", calculate_word_diversity(prideText))
+    print("Sherlock Holmes:", calculate_word_diversity(sherlockText))
+
+
+
+    print(search("love", books))
+    print(search("young man", books))
